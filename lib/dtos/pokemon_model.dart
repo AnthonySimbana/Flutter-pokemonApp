bf3df56680aac93668706dc5d8fa8a6493d5938a
Anthony Simbaña <90488409+AnthonySimbana@users.noreply.github.com>
2023-08-14 18:03:42 -0500

    Conversion de Documentos a Pokemons

--- lib/dtos/pokemon_model.dart
+++ lib/dtos/pokemon_model.dart
@@ -16,4 +16,24 @@ class Pokemon {
       imageUrl: json['sprites']['front_default'],
     );
   }
+
+  //Estos metodos para convertir los documentos en un dato de tipo específico en el modelo
+  //El withConverter se lo hace en la llamada
+
+//Esto para convertir lo que viene de Firebase a Pokemon
+  factory Pokemon.fromFirebaseJson(Map<String, dynamic> json) {
+    return Pokemon(
+      id: json['id'],
+      name: json['name'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+
+//Otra manera de expresar un return de manera abreviada con el () =>
+//Para convertir a Objeto lo que tiene el Pokemon
+  Map<String, Object?> toJson() => {
+        'id': id,
+        'name': name,
+        'imageUrl': imageUrl,
+      };
 }
