c73bade83fa34e18b5902c70507fb9e58f25f1cb
Anthony Simbaña <90488409+AnthonySimbana@users.noreply.github.com>
2023-08-14 18:27:41 -0500

    Modificacion de widget list

--- lib/widgets/pokemon_list.dart
+++ lib/widgets/pokemon_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_mobile/providers/pokemon_provider.dart';
-import 'package:pokedex_mobile/screens/pokemon_details.dart';
+import 'package:pokedex_mobile/widgets/pokemon_list_items.dart';
 import 'package:provider/provider.dart';
 
 class PokemonList extends StatefulWidget {
@@ -14,38 +14,9 @@ class _PokemonListState extends State<PokemonList> {
   @override
   Widget build(BuildContext context) {
     return Consumer<PokemonProvider>(builder: (context, provider, child) {
-      return ListView.builder(
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(3.0),
-            child: GestureDetector(
-              onTap: () => {
-                /*Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const PokemonDetailsScreen(),
-                  ),
-                ),
-                */
-                Navigator.pushNamed(context, PokemonDetailsScreen.routeName,
-                    arguments: provider.pokemons[index].id)
-              },
-              child: Card(
-                elevation: 10,
-                child: ListTile(
-                  leading: Hero(
-                      tag: provider.pokemons[index].id,
-                      child: Image.network(provider.pokemons[index].imageUrl)),
-                  title: Text(
-                    provider.pokemons[index].name,
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-        itemCount: provider.totalPokemons,
-      );
+      return PokemonListItems(
+          pokemons: provider
+              .pokemons); //Wiget que va a reenderizar el arreglo de pokemons
     });
   }
 }
